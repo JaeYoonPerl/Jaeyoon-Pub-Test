fd9db6b2a45292dd748274da6dd225bdfbc542f9
JaeYoonLee <a7750430@naver.com>
2024-11-21 17:15:27 +0900

    s

---
+++ DuobooTest/구현소스/js/common.js
@@ -0,0 +1,11 @@
+// common.js
+
+import myFn from "./my_function.js";
+
+export default function setElement(){
+    // 대상 선정
+    const topArea = myFn.qs("top-area");
+    const banArea = myFn.qs("#ban-area");
+    const footerArea = myFn.qs("#footer-area");
+
+}
---
+++ DuobooTest/구현소스/js/main.js
@@ -0,0 +1,9 @@
+// main.js
+
+// 공통함수
+import setElement from "./common.js";
+
+setElement(); // 함수호출
+
+// 내 함수 불러오기
+import myFn from "./my_function.js";
