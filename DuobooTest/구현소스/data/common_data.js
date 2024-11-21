1311262c783643da0644e0c18d657161011131c3
JaeYoonLee <a7750430@naver.com>
2024-11-21 21:21:30 +0900

    2

---
+++ DuobooTest/구현소스/data/common_data.js
@@ -0,0 +1,18 @@
+const comData ={
+
+    //1. 상단 영역 코드
+    topArea:`
+    <header class="top-area inbox">
+
+        <!-- 1-1. 최상위 -->
+        <div class ="top-box">
+            <!-- 1-1-1. 쇼핑몰 카테 -->
+            <div class="shopcate-box">
+            </div>
+            <!-- 1-1-2. 로드인 회원가입 -->
+            
+        </div>
+
+    </header>
+    `
+}
